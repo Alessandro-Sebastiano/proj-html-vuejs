24be6020dd23ec1fbaa159c2f1f5d24c159e7eb4
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2022-11-21 18:46:50 +0100

    main-component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,24 @@
 <template>
   <HeaderComponent />
+  <MainComponent />
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
+import MainComponent from './components/MainComponent.vue';
+
 
 export default {
   components: {
     HeaderComponent,
+    MainComponent,
   }
 }
 </script>
 
-<style lang="scss" scoped>
-
+<style lang="scss">
+.container {
+  width: 1105px;
+  margin: auto;
+}
 </style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="main">
+        <HeroComponent />
+    </div>
+</template>
+
+<script>
+import HeroComponent from './main-subComponent/HeroComponent.vue';
+
+export default {
+    name: 'MainComponent',
+
+    components: {
+        HeroComponent,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.main {
+    width: 100%;
+}
+</style>
