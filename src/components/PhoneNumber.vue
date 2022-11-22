d65835d7dc8a9fd28a2b69cc89afc6c83a08f794
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2022-11-22 15:57:05 +0100

    review

--- src/components/PhoneNumber.vue
+++ src/components/PhoneNumber.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="phone-box">
-        <span><i class="fa-solid fa-phone"></i></span>
-        <span>(555) 802-1234</span>
+        <a href="#">
+            <span><i class="fa-solid fa-phone"></i>(555) 802-1234</span>
+        </a>
     </div>
 </template>
 
@@ -16,13 +17,10 @@ export default {
 
 .phone-box {
     padding: 6px 18px;
-    display: flex;
-    align-items: center;
     border-radius: 8px;
     font-weight: 600;
 
     span {
-        display: block;
 
         i {
             margin-right: 12px;
