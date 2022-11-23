15729510f4a8fa5f403fdc0ea483ab1f27d58f15
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2022-11-23 16:14:03 +0100

    case-card-overlay

--- src/components/ResourceCard.vue
+++ src/components/ResourceCard.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="resource-card">
         <div class="resource-card-image">
+            <div class="overlay"></div>
             <img :src="'/images/' + resourceImg" alt="resource-image">
         </div>
-        <span>{{ resourceText }}</span>
+        <span><a href="#">{{ resourceText }}</a></span>
     </div>
 </template>
 
@@ -23,6 +24,25 @@ export default {
     .resource-card-image {
         width: 100%;
         cursor: pointer;
+        position: relative;
+
+        .overlay {
+            position: absolute;
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: 0;
+            background: rgb(255, 255, 255);
+            background: linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(226, 92, 14, 0.3785889355742297) 100%);
+            opacity: 0;
+            transition: all 0.6s ease-in-out;
+        }
+
+        &:hover {
+            .overlay {
+                opacity: 1;
+            }
+        }
 
 
         img {
